ae51f7b1c34d6272337e88b14c27c951e2c7921b
VclhOfGamer <157661369+VclhOfGamer@users.noreply.github.com>
2024-11-07 11:12:10 +0700

    Add files via upload

--- ltweb/movie.py
+++ ltweb/movie.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, g,redirect,session,url_for,flash
 import sqlite3
 
 app = Flask(__name__)
 DATABASE = 'cinema.db'
+app.secret_key = '1234'
 
 # Kết nối SQLite
 def get_db():
@@ -59,6 +60,74 @@ def movie_detail(movie_id):
     if movie is None:
         return "Movie not found", 404
     return render_template('chitietphim.html', movie=movie)
+
+# Route hiển thị Login và Signup
+@app.route('/login_page')
+def login_page():
+    return render_template('login.html')
+
+# Route login
+@app.route('/login', methods=['POST'])
+def login_user():
+    customerEmail = request.form['customerEmail']
+    customerPassword = request.form['customerPassword']
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT customerName, customerID FROM Customer WHERE customerEmail = ? AND customerPassword = ?', 
+                   (customerEmail, customerPassword))
+    user = cursor.fetchone()
+    conn.close()
+    
+    if user:
+        session['customerName'] = user['customerName']
+        session['customerID'] = user['customerID']
+        print(f"Logged in as: {session['customerName']}") 
+        return redirect(url_for('homepage'))
+    else:
+        session['alert_message'] = 'Thông tin đăng nhập không chính xác, vui lòng nhập lại'
+        return redirect(url_for('login_page', form='login'))
+
+
+# Route signup
+@app.route('/signup', methods=['POST'])
+def signup_user():
+    username = request.form['username']
+    email = request.form['email']
+    phone = request.form['phone']
+    password = request.form['password']
+
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO Customer (customerName, customerEmail, phoneNumber, customerPassword) VALUES (?, ?, ?, ?)", 
+                   (username, email, phone, password))
+    conn.commit()
+    conn.close()
+
+    session['customerEmail'] = email 
+    return redirect(url_for('homepage'))
+
+# Route logout
+@app.route('/logout')
+def logout():
+    session.clear()  
+    return redirect(url_for('homepage')) 
+
+# Route rap
+@app.route('/rap')
+def rap():
+    return render_template('rap.html')
+
+# Route my ticket
+@app.route('/myticket')
+def myticket():
+    db = get_db()
+    customer_id = session.get('customerID')
+    if not customer_id:
+        return redirect(url_for('login_page'))  
+    
+    tickets = db.execute("SELECT * FROM Ticket WHERE customerID = ?", (customer_id,)).fetchall()
+    return render_template('myticket.html', tickets=tickets)
 
 if __name__ == '__main__':
     app.run(debug=True)
